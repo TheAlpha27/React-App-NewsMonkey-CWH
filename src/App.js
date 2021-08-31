33f10262594435d74e6b3bce9d8b4eed4d641d50
TheAlpha27 <utsav.soni.27@gmail.com>
2021-08-31 21:34:24 +0530

    did some minor changes

--- src/App.js
+++ src/App.js
@@ -10,15 +10,18 @@ export default class App extends Component {
       <>
         <Navbar />
         <div className="container py-3">
-          <div className="row">
-            <div className="col-md-4">
-              <News title='Title Here' description='Description here' />
+          <div className="row" style={{justifyContent:'center', alignItems:'center', display:'flex'}}>
+            <div className="col-12">
+              <h1 style={{textAlign:'center'}}>News Headlines!</h1>
             </div>
-            <div className="col-md-4">
-              <News title='Title Here' description='Description here' />
+            <div className="col-md-4" style={{justifyContent:'center', alignItems:'center', display:'flex'}}>
+              <News title='Title Here' description='Description here' ImageURL='https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80' />
             </div>
-            <div className="col-md-4">
-              <News title='Title Here' description='Description here' />
+            <div className="col-md-4" style={{justifyContent:'center', alignItems:'center', display:'flex'}}>
+              <News title='Title Here' description='Description here' ImageURL='https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80' />
+            </div>
+            <div className="col-md-4" style={{justifyContent:'center', alignItems:'center', display:'flex'}}>
+              <News title='Title Here' description='Description here' ImageURL='https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80' />
             </div>
           </div>
         </div>
--- src/Components/News.js
+++ src/Components/News.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 export class News extends Component {
     render() {
-        let {title, description} = this.props;
+        let {title, description, ImageURL} = this.props;
         return (
             <div class="card my-3 mx-3" style={{width: '18rem'}}>
-                <img src="..." class="card-img-top" alt="..." />
+                <img src={ImageURL} alt="..." />
                 <div class="card-body">
                     <h5 class="card-title">{title}</h5>
                     <p class="card-text">{description}.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <a href="#" class="btn btn-sm btn-primary">Read More...</a>
                 </div>
             </div>
         )

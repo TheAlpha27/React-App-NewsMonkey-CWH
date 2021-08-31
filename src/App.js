73a1b70430e96850bb48277f16328980e8da29a1
TheAlpha27 <utsav.soni.27@gmail.com>
2021-08-31 21:54:28 +0530

    Added sample json data, created a constructor in news.js, separated the css for news card in index.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-
+import './index.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
@@ -10,18 +9,18 @@ export default class App extends Component {
       <>
         <Navbar />
         <div className="container py-3">
-          <div className="row" style={{justifyContent:'center', alignItems:'center', display:'flex'}}>
+          <div className="row news-container">
             <div className="col-12">
               <h1 style={{textAlign:'center'}}>News Headlines!</h1>
             </div>
-            <div className="col-md-4" style={{justifyContent:'center', alignItems:'center', display:'flex'}}>
+            <div className="col-md-4 news-card-container">
               <News title='Title Here' description='Description here' ImageURL='https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80' />
             </div>
-            <div className="col-md-4" style={{justifyContent:'center', alignItems:'center', display:'flex'}}>
-              <News title='Title Here' description='Description here' ImageURL='https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80' />
+            <div className="col-md-4 news-card-container">
+              <News title='Title Here' description='Description here'/>
             </div>
-            <div className="col-md-4" style={{justifyContent:'center', alignItems:'center', display:'flex'}}>
-              <News title='Title Here' description='Description here' ImageURL='https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80' />
+            <div className="col-md-4 news-card-container">
+              <News title='Title Here' description='Description here'/>
             </div>
           </div>
         </div>
